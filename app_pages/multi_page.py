1ab1b0700a75be30a3595c8cbf735f5593e11fc1
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-06-30 17:39:42 +0000

    Page confifg for multipage app

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -1,1 +1,20 @@
+import streamlit as st
+
+class MultiPage:
+    
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+        st.set_page_config(
+            page_title = self.app_name,
+            page_icon = ":material/thumb_up:"
+        )
+
+    def app_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func}) # It is a dictionary
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio("Menu", self.pages, format_func=lambda page: page['title'] )
+
 
--- app_pages/page2.py
+++ app_pages/page2.py
@@ -1,1 +1,5 @@
+import streamlit as st
+
+def page2_body():
+    st.write('This is Page 2 | 2')
 
